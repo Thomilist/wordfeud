87cba0b59ee870448c48520bacfc20433c02ebf7
Thomas Emil Jensen <Thomas_98@hotmail.dk>
2023-06-14 14:57:25 +0200

    Version number increment

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@ int main(int argc, char* argv[])
 
     QCoreApplication::setOrganizationName("Thomilist");
     QCoreApplication::setApplicationName("wordfeud");
-    QCoreApplication::setApplicationVersion("0.2.0");
+    QCoreApplication::setApplicationVersion("0.2.1");
 
     QFile stylesheet{":/dark/stylesheet.qss"};
     stylesheet.open(QFile::ReadOnly | QFile::Text);
